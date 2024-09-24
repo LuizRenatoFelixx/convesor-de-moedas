7deadd88ea720bab9530bf4006fb11bd68aa458c
LuizRenatoFelixx <luiz.2321190431021@aluno.eteprc.faetec.rj.gov.br>
2024-09-24 09:22:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function converter() {
+    const valor = parseFloat(document.getElementById("valor").value); // Obter o valor inserido
+    const opcao = document.getElementById("opcao").value; // Obter a opção selecionada
+    let resultado;
+
+    // Taxas de câmbio fixas (para um sistema real, idealmente essas taxas seriam dinâmicas)
+    const taxaRealParaDolar = 0.20; // 1 Real = 0,20 Dólar
+    const taxaRealParaEuro = 0.18
+}
